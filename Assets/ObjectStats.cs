dbbfaa13924492308b9635c77920374c3b25afdc
Zlabravac <david.valent15@gmail.com>
2024-12-06 07:17:29 +0100

    ne radi
    This reverts commit 6da4c51dd604b6285ea0fc3859a82e28c0b6dab5.

---
+++ Assets/ObjectStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ObjectStats : MonoBehaviour
+{
+    public Sprite photo;         // Photo of the object
+    public int storageCapacity;  // Storage capacity of the object
+    public float size;           // Size of the object
+}
